05ba85d464504d86cca61f380e437e15c0256258
Miriam <longoyapusmiriam@gmail.com>
2023-05-22 23:55:40 +0300

    commit

--- bank.py
+++ bank.py
@@ -1,1 +1,14 @@
 #python
+class Student:
+    name="Miriam"
+    age=27
+    school="Akirachix"
+    nationality="Kenyan"
+
+    def __init__(self,name,age,nationality):
+     self.name=name
+     self.age=age
+     self.nationality=nationality
+
+    def greet_student(self):
+     return f"Hello{Student.name}, welcome to {self.school}, proudly{self.nationality}"
